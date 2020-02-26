1f95fbbcf434b90257c1b3d275d793abbaf22d52
Fineas Anton <fineas.anton@gmail.com>
2020-02-26 11:09:35 -0600

    basic interval changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container d-flex justify-content-center">
+    <div class="row">
+      <div class="group-1" :class="{ christmasGreen: group1Active }"></div>
+      <div class="group-2" :class="{ christmasRed: group2Active }"></div>
+      <div class="group-1" :class="{ christmasGreen: group1Active }"></div>
+      <div class="group-2" :class="{ christmasRed: group2Active }"></div>
+      <div class="group-1" :class="{ christmasGreen: group1Active }"></div>
+      <div class="group-2" :class="{ christmasRed: group2Active }"></div>
+      <div class="group-1" :class="{ christmasGreen: group1Active }"></div>
+      <button
+        class="btn btn-primary"
+        style="margin-left: 1.5em;"
+        @click="startLightShow"
+      >
+        Start Light Show
+      </button>
+      <input type="text" v-model="timerIntensity" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      timerIntensity: 2000,
+      timerStarted: false,
+      group1Active: true,
+      group2Active: false
+    };
+  },
+  methods: {
+    startLightShow() {
+      this.timerStarted = !this.timerStarted;
+      let timer = setInterval(() => {
+        if (!this.timerStarted) {
+          clearInterval(timer);
+          return;
+        }
+        this.testNum++;
+        this.group1Active = !this.group1Active;
+        this.group2Active = !this.group2Active;
+      }, this.timerIntensity);
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.group-1,
+.group-2 {
+  height: 25px;
+  width: 25px;
+  background-color: #bbb;
+  border-radius: 50%;
+}
+
+.christmasGreen {
+  background-color: #4caf50;
+}
+.christmasRed {
+  background-color: #ff3333;
+}
+
+.container {
+  min-height: 100%; /* Fallback for browsers do NOT support vh unit */
+  min-height: 100vh; /* These two lines are counted as one :-)       */
+
+  display: flex;
+  align-items: center;
 }
 </style>
+
